f3e3c01e3279c6e3a404d3e63ed66931218551e7
sagargurunggg <200458207@student.georgianc.on.ca>
2021-03-02 17:34:53 -0500

    changed the design of addgenre.xaml & genrepage.xaml

--- Data Binding/Data Binding/Views/AddGenre.xaml.cs
+++ Data Binding/Data Binding/Views/AddGenre.xaml.cs
@@ -44,7 +44,50 @@ namespace Data_Binding.Views
                 selectedGenre.Remove("Action");
             }
         }
-
+        private void Animation_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Animation");
+            }
+            else
+            {
+                selectedGenre.Remove("Animation");
+            }
+        }
+        private void Adventure_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Adventure");
+            }
+            else
+            {
+                selectedGenre.Remove("Adventure");
+            }
+        }
+        private void Biography_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Biography");
+            }
+            else
+            {
+                selectedGenre.Remove("Biography");
+            }
+        }
+        private void Costume_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Costume");
+            }
+            else
+            {
+                selectedGenre.Remove("Costume");
+            }
+        }
         private void Comedy_CheckedChanged(object sender, CheckedChangedEventArgs e)
         {
             if (e.Value)
@@ -54,6 +97,161 @@ namespace Data_Binding.Views
             else
             {
                 selectedGenre.Remove("Comedy");
+            }
+        }
+        private void Romance_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Romance");
+            }
+            else
+            {
+                selectedGenre.Remove("Romance");
+            }
+        }
+        private void Horror_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Horror");
+            }
+            else
+            {
+                selectedGenre.Remove("Horror");
+            }
+        }
+        private void Scifi_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Scifi");
+            }
+            else
+            {
+                selectedGenre.Remove("Scifi");
+            }
+        }
+        private void Documentary_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Documentary");
+            }
+            else
+            {
+                selectedGenre.Remove("Documentary");
+            }
+        }
+
+        private void Crime_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Crime");
+            }
+            else
+            {
+                selectedGenre.Remove("Crime");
+            }
+        }
+        private void Drama_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Drama");
+            }
+            else
+            {
+                selectedGenre.Remove("Drama");
+            }
+        }
+        private void Family_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Family");
+            }
+            else
+            {
+                selectedGenre.Remove("Family");
+            }
+        }
+        private void Fantasy_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Fantasy");
+            }
+            else
+            {
+                selectedGenre.Remove("Fantasy");
+            }
+        }
+        private void Gameshow_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Game-show");
+            }
+            else
+            {
+                selectedGenre.Remove("Game-show");
+            }
+        }
+        private void History_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("History");
+            }
+            else
+            {
+                selectedGenre.Remove("History");
+            }
+        }
+        private void Kungfu_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Kungfu");
+            }
+            else
+            {
+                selectedGenre.Remove("Kungfu");
+            }
+        }
+        private void Music_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Music");
+            }
+            else
+            {
+                selectedGenre.Remove("Music");
+            }
+        }
+        private void Sports_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Sports");
+            }
+            else
+            {
+                selectedGenre.Remove("Sports");
+            }
+        }
+        private void Thriller_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                selectedGenre.Add("Thriller");
+            }
+            else
+            {
+                selectedGenre.Remove("Thriller");
             }
         }
     }
