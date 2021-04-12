a7f9722dc098a79189e8188051c178344aa50efd
Brenden <brenden_kevin@hotmail.com>
2021-04-12 15:14:19 -0400

    server based login verification

---
+++ Data Binding/Data Binding/Services/NetworkCalls.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace Data_Binding.Services
+{
+    public class NetworkCalls
+    {
+        public NetworkCalls()
+        {
+        }
+
+        public string Login(string username, string password)
+        {
+            var client = new RestClient("https://content-guide.herokuapp.com/login");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", "{\n    \"email_address\" : " + username + ",\n    \"password\" : " + password + "\n}", ParameterType.RequestBody);
+            Console.WriteLine(request);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            JObject joResponse = JObject.Parse(response.Content);
+            JObject ojObject = (JObject)joResponse;
+            string token = (string)ojObject["token"];
+            return token;
+        }
+    }
+}
